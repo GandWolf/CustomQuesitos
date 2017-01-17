3e8ce03b16f7c9bd15a84df9c3e6448340399b1b
gand <zaken85@gmail.com>
2017-01-17 10:09:19 +0100

    Quesitos 2

--- app/src/main/java/com/example/gand/customquesitos/MainActivity.java
+++ app/src/main/java/com/example/gand/customquesitos/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DiagramaQueso pie = (DiagramaQueso) findViewById(R.id.diagrama);
+        float[] data = {450, 630, 300, 200, 400};
+        pie.setData(data);
     }
 }
