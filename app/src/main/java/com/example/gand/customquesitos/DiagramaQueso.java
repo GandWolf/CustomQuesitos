8c30ecec9f1bf1ee6268a7429b4f69155ab8a3bc
gand <zaken85@gmail.com>
2017-01-19 09:49:34 +0100

    Quesitos 2

--- app/src/main/java/com/example/gand/customquesitos/DiagramaQueso.java
+++ app/src/main/java/com/example/gand/customquesitos/DiagramaQueso.java
@@ -16,77 +16,85 @@ import java.util.Random;
 
 public class DiagramaQueso extends View {
 
-    private Paint piePaint;
+    private Paint quesoPaint, circuloPaint;
     private RectF rectF;
-    private float[] data;
+    private float arrayDatos[];
+    private int arrayColor[];
 
-    int top, left, endBottom, endRight;
+    int top, left, endBottom, endRight, diametro, grosor;
 
     public DiagramaQueso(Context context, AttributeSet attrs){
         super(context,attrs);
 
-        piePaint = new Paint();
-        piePaint.setAntiAlias(true);
-        piePaint.setDither(true);
-        piePaint.setStyle(Paint.Style.FILL);
+        quesoPaint = new Paint();
+        quesoPaint.setAntiAlias(true);
+        quesoPaint.setDither(true);
+        quesoPaint.setStyle(Paint.Style.FILL);
+//        quesoPaint.setShadowLayer(20,20,20,0xFF000000);
+
+        circuloPaint = new Paint();
+        circuloPaint.setStyle(Paint.Style.STROKE);
+        circuloPaint.setColor(Color.argb(255, 0, 0, 0));
+        grosor = 10;
+        circuloPaint.setStrokeWidth(grosor);
 
 
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // Determine horizontal and vertical padding
-        int paddingX = getPaddingLeft() + getPaddingRight();
-        int paddingY = getPaddingBottom() + getPaddingTop();
-
-        int minW, minH, w, h;
-        switch (MeasureSpec.getMode(heightMeasureSpec)) {
-            case MeasureSpec.EXACTLY:
-                // Try for a height based on our minimum including vertical padding
-                minH = getSuggestedMinimumHeight() + paddingY;
-                h = MeasureSpec.getSize(heightMeasureSpec);
-
-                // Set the width according to the height as our control should be
-                // square, again compensating for padding
-                minW = MeasureSpec.getSize(h) - paddingY + paddingX;
-                w = resolveSize(minW, widthMeasureSpec);
-                break;
-            default:
-                // Try for a width based on our minimum including horizontal padding
-                minW = getSuggestedMinimumWidth() + paddingX;
-                w = resolveSize(minW, widthMeasureSpec);
-
-                // Set the height according to the width as our control should be
-                // square, again compensating for padding
-                minH = MeasureSpec.getSize(w) - paddingX + paddingY;
-                h = resolveSize(minH, heightMeasureSpec);
-                break;
-        }
-
-        setMeasuredDimension(w, h);
-    }
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        // Determine horizontal and vertical padding
+//        int paddingX = getPaddingLeft() + getPaddingRight() + grosor;
+//        int paddingY = getPaddingBottom() + getPaddingTop() + grosor;
+//
+//        int minW, minH, w, h;
+//        switch (MeasureSpec.getMode(heightMeasureSpec)) {
+//            case MeasureSpec.EXACTLY:
+//                // Try for a height based on our minimum including vertical padding
+//                minH = getSuggestedMinimumHeight() + paddingY + grosor;
+//                h = MeasureSpec.getSize(heightMeasureSpec);
+//
+//                // Set the width according to the height as our control should be
+//                // square, again compensating for padding
+//                minW = MeasureSpec.getSize(h) - paddingY + paddingX;
+//                w = resolveSize(minW, widthMeasureSpec);
+//                break;
+//            default:
+//                // Try for a width based on our minimum including horizontal padding
+//                minW = getSuggestedMinimumWidth() + paddingX;
+//                w = resolveSize(minW, widthMeasureSpec);
+//
+//                // Set the height according to the width as our control should be
+//                // square, again compensating for padding
+//                minH = MeasureSpec.getSize(w) - paddingX + paddingY;
+//                h = resolveSize(minH, heightMeasureSpec);
+//                break;
+//        }
+//
+//        setMeasuredDimension(w, h);
+//    }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         // Account for padding
-        left = getPaddingLeft();
-        top = getPaddingTop();
-        int diameter = Math.min(
-                w - left - getPaddingRight(),
-                h - top - getPaddingBottom());
-        endRight = left + diameter;
-        endBottom= top + diameter;
+        left = getPaddingLeft() + grosor/2;
+        top = getPaddingTop() + grosor/2;
+        diametro = Math.min(
+                w - left - getPaddingRight() - grosor/2,
+                h - top - getPaddingBottom() - grosor/2);
+        endRight = left + diametro;
+        endBottom= top + diametro;
     }
 
     private float[] pieSegment(){
 
-        float[] segValues = new float[this.data.length];
+        float[] segValues = new float[this.arrayDatos.length];
         float Total = getTotal();
 
-        for (int i = 0; i < this.data.length; i++){
+        for (int i = 0; i < this.arrayDatos.length; i++){
 
-            segValues[i] = (this.data[i]/Total) * 360;
+            segValues[i] = (this.arrayDatos[i]/Total) * 360;
         }
 
         return segValues;
@@ -97,7 +105,7 @@ public class DiagramaQueso extends View {
 
         float total = 0;
 
-        for (float val : this.data){
+        for (float val : this.arrayDatos){
             total +=val;
         }
 
@@ -107,30 +115,31 @@ public class DiagramaQueso extends View {
     @Override
     protected void onDraw(Canvas canvas){
 
-        if (data != null){
-
+        if (arrayDatos != null){
 
             rectF = new RectF(left, top, endRight, endBottom);
 
             float[] segment = pieSegment();
-
             float segStartPoint = 0;
 
+
+
             for (int i = 0; i < segment.length; i++){
-
-                Random rnd = new Random();
-                int color = Color.argb(255, (int)segment[i], rnd.nextInt(256), rnd.nextInt(256));
-
-                piePaint.setColor(color);
-                canvas.drawArc(rectF, segStartPoint, segment[i], true, piePaint);
+//                Random rnd = new Random();
+//                int color = Color.argb(255, (int)segment[i], rnd.nextInt(256), rnd.nextInt(256));
+//                quesoPaint.setColor(color);
+                quesoPaint.setColor(arrayColor[i]);
+                canvas.drawArc(rectF, segStartPoint, segment[i], true, quesoPaint);
                 segStartPoint += segment[i];
             }
+            canvas.drawCircle(left+(diametro/2),(top + diametro/2),(diametro/2),circuloPaint);
         }
     }
 
-    public void setData(float[] data){
+    public void setArrayDatos(float[] arrayDatos, int [] arrayColor){
 
-        this.data = data;
+        this.arrayDatos = arrayDatos;
+        this.arrayColor = arrayColor;
         invalidate();
     }
 }//Fin class
--- app/src/main/java/com/example/gand/customquesitos/MainActivity.java
+++ app/src/main/java/com/example/gand/customquesitos/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DiagramaQueso pie = (DiagramaQueso) findViewById(R.id.diagrama);
-        float[] data = {450, 630, 300, 200, 400};
-        pie.setData(data);
+        float [] data = {450, 630, 300, 200, 400, 555, 50};
+        int [] color = {0xFF110000,0xFF330000,0xFF550000,0xFF770000,0xFF990000,0xFFbb0000,0xFFdd0000};
+        pie.setArrayDatos(data, color);
     }
 }
