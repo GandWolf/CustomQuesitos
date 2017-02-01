e37ab6025a2f00e1c0e14283a5276644710f4d40
Gand <zaken85@gmail.com>
2017-02-01 20:41:57 +0100

    Gand Ejemplo

--- app/src/main/java/com/example/gand/customquesitos/MainActivity.java
+++ app/src/main/java/com/example/gand/customquesitos/MainActivity.java
@@ -27,20 +27,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        Random rnd = new Random();
+//
+//        main = (RelativeLayout) findViewById(R.id.activity_main);
+//        queso = (DiagramaQueso) findViewById(R.id.diagrama);
+//        float [] data = {rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75)};
+////        float [] data = {rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75)};
+////        int [] color = {0xFF110000,0xFF330000,0xFF550000,0xFF770000,0xFF990000,0xFFbb0000,0xFFdd0000};
+//        String [] nombre = {"AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III", "JJJ"};
+////        String [] nombre = {"AAA", "BBB", "CCC", "DDD", "EEE"};
+////        float [] data = {1,5,3};
+//
+////        float [] data = {50, 50, 50, 50, 50, 50, 50, 50};
+////        String [] nombre = {"000","111", "222", "333", "444","555","666","777"};
+//        queso.setArrayDatos(data, nombre);
 
-        Random rnd = new Random();
-
-        main = (RelativeLayout) findViewById(R.id.activity_main);
-        queso = (DiagramaQueso) findViewById(R.id.diagrama);
-        float [] data = {rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75)};
-//        float [] data = {rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75),rnd.nextInt(75)};
-//        int [] color = {0xFF110000,0xFF330000,0xFF550000,0xFF770000,0xFF990000,0xFFbb0000,0xFFdd0000};
-        String [] nombre = {"AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III", "JJJ"};
-//        String [] nombre = {"AAA", "BBB", "CCC", "DDD", "EEE"};
-//        float [] data = {1,5,3};
-
-//        float [] data = {50, 50, 50, 50, 50, 50, 50, 50};
-//        String [] nombre = {"000","111", "222", "333", "444","555","666","777"};
-        queso.setArrayDatos(data, nombre);
+//        Ejemplo ej = (Ejemplo) findViewById(R.id.ejemplo);
+//        ej.pintar();
     }
 }
